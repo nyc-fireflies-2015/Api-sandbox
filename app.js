2cf96fc50c5682d9476e461e2b19f3d5f261d8c8
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-15 17:29:05 -0400

    Fix posts array to return new post objects

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get("http://api.reddit.com").success(function(response) {
-    $scope.posts = response.data.children;
-    console.log($scope.posts);
+    $scope.posts = response.data.children.map(function(post){
+      return new Post(post.data);
+    });
+  console.log($scope.posts);
   })
+
+
 
 }]);
