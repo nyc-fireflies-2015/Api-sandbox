63defa061e156b55c83395c69988392cf5e5ed40
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-15 15:44:55 -0400

    Remove old jquery and add angular, console log hi

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
-$(document).ready(function() {
-  $("button").on("click", function() {
-    $.ajax("http://api.reddit.com").done(function(response) {
-      var templateScript = $("#reddit-template").html();
-      var theTemplate = Handlebars.compile(templateScript);
-      debugger
-      response.data.children.forEach(function(listing){
-        console.log(listing.data.title)
-        var context = {
-          post: [
-            { title: listing.data.title }
-          ]
-        };
-      var theCompiledHtml = theTemplate(context);
-      $(document.body).append(theCompiledHtml);
-      })
-
-
-      console.log(response);
-    }).fail(function(response) {
-      alert("Error");
-      console.log(response);
-    })
-  })
-})
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+  console.log("HI");
+});
