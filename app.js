cb55362ed16e2a4cb6d5838b23cfbcafe8c71353
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-15 12:03:42 -0400

    Add ajax call and working handlebars script

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-  console.log('HI');
+  $("button").on("click", function() {
+    $.ajax("http://api.reddit.com").done(function(response) {
+      var templateScript = $("#reddit-template").html();
+      var theTemplate = Handlebars.compile(templateScript);
+      debugger
+      response.data.children.forEach(function(listing){
+        console.log(listing.data.title)
+        var context = {
+          post: [
+            { title: listing.data.title }
+          ]
+        };
+      var theCompiledHtml = theTemplate(context);
+      $(document.body).append(theCompiledHtml);
+      })
+
+
+      console.log(response);
+    }).fail(function(response) {
+      alert("Error");
+      console.log(response);
+    })
+  })
 })
